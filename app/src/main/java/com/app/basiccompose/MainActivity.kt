df2958101d64b0034334bf6ff11ca5637006e8cf
prakashdev2 <mprakashdev2@gmail.com>
2022-02-06 00:40:07 +0530

    superscript text completed

--- app/src/main/java/com/app/basiccompose/MainActivity.kt
+++ app/src/main/java/com/app/basiccompose/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.basiccompose.ui.theme.BasicComposeTheme
@@ -121,17 +122,20 @@ fun Greeting() {
     @Composable
     fun SuperScriptText(normalText : String,
                         superText : String,
+                        normalFontSize : TextUnit = MaterialTheme.typography.subtitle1.fontSize,
+                        superFontSize : TextUnit = MaterialTheme.typography.overline.fontSize,
+                        superFontWeight : FontWeight = FontWeight.Normal
     ){
         Text(buildAnnotatedString {
             withStyle(style = SpanStyle(
-                fontSize = MaterialTheme.typography.subtitle1.fontSize
+                fontSize = normalFontSize
             )
             ){
                 append(normalText)
             }
             withStyle(style = SpanStyle(
-                fontSize = MaterialTheme.typography.overline.fontSize,
-                fontWeight = FontWeight.Normal,
+                fontSize = superFontSize,
+                fontWeight = superFontWeight,
                 baselineShift = BaselineShift.Superscript
             )
             ){
@@ -146,7 +150,12 @@ fun Greeting() {
 @Composable
 fun DefaultPreview() {
     BasicComposeTheme {
-      SuperScriptText(normalText = "Hello", superText = "World")
+        Column(modifier = Modifier.fillMaxSize()) {
+            SuperScriptText(normalText = "Hello", superText = "World",
+            superFontWeight = FontWeight.Light
+            )
+        }
+
     }
 }
 
